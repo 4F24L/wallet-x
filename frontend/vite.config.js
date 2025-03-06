52016daf0878629cea2138f95a0dfcddaeaeb4d6
4F24L <afzalmir2003@gmail.com>
2025-03-06 15:01:33 +0530

    lottie fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,9 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: { port: 5173, host: "0.0.0.0" },
+  optimizeDeps: {
+    exclude: ["lottie-web"], // Exclude lottie-web from optimization
+  },
   build: {
     chunkSizeWarningLimit: 1000, // Increase chunk size warning limit
     rollupOptions: {

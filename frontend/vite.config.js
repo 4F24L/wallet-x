ae95913ce0e4813597104950b4e875be76024cec
4F24L <afzalmir2003@gmail.com>
2025-03-08 05:44:13 +0530

    vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,22 +4,4 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    port: 5173,
-    host: "0.0.0.0",
-  },
-  build: {
-    outDir: "dist",
-    assetsDir: "assets",
-    chunkSizeWarningLimit: 1000, // Prevents warnings on large files
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return "vendor"; // Splits vendor files to avoid large chunks
-          }
-        }
-      }
-    }
-  }
 });

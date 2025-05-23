60d14e7ede8aec9bef8a5f2b2c96bc8f589e7ccc
4F24L <afzalmir2003@gmail.com>
2025-05-23 23:28:07 +0530

    cors fix

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,40 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mainRouter = require('./routes/index');
+const mainRouter = require("./routes/index");
 const port = Number(process.env.PORT) || 3000;
-const cors = require('cors');
-const dotenv = require('dotenv');
+const cors = require("cors");
+const dotenv = require("dotenv");
 
 dotenv.config();
 app.use(express.json());
 
-app.use(cors({
-  origin: [ "https://wallet-x-app.vercel.app", "https://walletx.afzaldev.in","https://walletx.afzalmir.me", "http://localhost:5173", ],
-  methods : [ 'GET', 'POST', 'PUT', 'DELETE' ],
-  credentials: true,  // allow session cookies from browser to pass through
-}));
+const allowedOrigins = [
+  "https://wallet-x-app.vercel.app",
+  "https://walletx.afzaldev.in",
+  "https://walletx.afzalmir.me",
+  "http://localhost:5173",
+];
 
-
-
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use("/api/v1", mainRouter);
 
-app.get('/', (req, res) => {
-    res.send('Hello to Wallet-X !');
+app.get("/", (req, res) => {
+  res.send("Hello to Wallet-X !");
 });
 
-app.listen(port , "0.0.0.0", () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server is running on port http://localhost:${port}`);
 });
